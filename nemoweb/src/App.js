6ad0b33f0cc461ab2f51f9294dcd130bcc469661
wentaog97 <wentaog@hotmail.com>
2023-02-17 14:14:21 -0800

    Modified 02/17/2023

--- nemoweb/src/App.js
+++ nemoweb/src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div className="App">
       <div className="Background">
         <div className="App-Frame">
-
           <header className="App-header">
             <div className="app-title">
               <h3>Evolutionary Dynamics</h3>
@@ -15,42 +14,66 @@ function App() {
           <hr className="divider"></hr>
 
           <body className="App-body">
-            
-            <div className="body file">
-              <fieldset>
-                <legend>Load a file</legend>
-                <label>Select File:</label>
-                <input></input>
-                <button>Browse</button>
-                
-                <label>File:</label>
-              </fieldset>
+            <div className="body-toggle">
+              {bodyFile()}
+              {bodyPlot()}
+              {bodyFilter()}
+              {bodyCluster()}
             </div>
 
-            <div className="body plot">
-              <fieldset>
-                <legend>Frequency plot</legend>
-              </fieldset>
-            </div>
+            <div className="body-graph">
 
-            <div className="body filter">
-              <fieldset>
-                <legend>Pre-filtering</legend>
-              </fieldset>
-            </div>
-
-            <div className="body cluster">
-              <fieldset>
-                <legend>Clustering</legend>
-              </fieldset>
             </div>
 
           </body>
-
         </div>
       </div>
     </div>
   );
 }
+
+
+function bodyFile(){
+return (
+  <div className="body file">
+    <fieldset>
+      <legend>Load a file</legend>
+      <label>Select File:</label>
+      <input></input>
+      <button>Browse</button>
+      <label>File:</label>
+    </fieldset>
+  </div>
+);
+};
+function bodyPlot(){
+  return (
+    <div className="body plot">
+      <fieldset>
+        <legend>Frequency plot</legend>
+      </fieldset>
+    </div>
+  );
+};
+function bodyFilter(){
+  return (
+    <div className="body filter">
+      <fieldset>
+        <legend>Pre-filtering</legend>
+      </fieldset>
+    </div>
+  );
+};
+function bodyCluster(){
+  return (
+    <div className="body cluster">
+      <fieldset>
+        <legend>Clustering</legend>
+      </fieldset>
+    </div>
+  );
+};
+
+
 
 export default App;
